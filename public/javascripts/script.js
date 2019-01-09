7d4a35654f7889317aff0fe339a31723ded997ad
Sandrico Provo <Sprovo@outlook.com>
2019-01-09 06:28:15 -0400

    Fix issue where adding style in JS is not working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,10 +2,8 @@ const textArea = document.querySelector(".paragraph--textarea-rolled");
 
 function diceRoll(min = 1, max = 7) {
 	let roll = Math.floor(Math.random() * (max - min) + 1);
+	let rolledDice = document.querySelector(`div[data-key="${roll}"]`);
+	console.log(rolledDice);
+	// rolledDice.style.classList.add("dice-active");
 	textArea.innerHTML = `You rolled a ${roll}`;
-	if (roll <= 6) {
-		dice = document.querySelector(`div--dice-${roll}`);
-		dice.style.classList.add("dice-active");
-	}
-	console.log(roll);
 }
