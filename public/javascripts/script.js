4a461bdb6036cc6ef039b03db70e06f3afaf62c6
Sandrico Provo <Sprovo@outlook.com>
2019-01-11 16:36:07 -0400

    Finish JavaScript file, start coding in more CSS

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,19 @@
 const textArea = document.querySelector(".paragraph--textarea-rolled");
+const rollButton = document.querySelector(".button--dice-roll");
 
-function diceRoll(min = 1, max = 7) {
-	let roll = Math.floor(Math.random() * (max - min) + 1);
+rollButton.addEventListener("click", function() {
+	// Use a randomly generated number between 1-6 to target image that matches rolled dice
+	let roll = Math.floor(Math.random((min = 1), (max = 7)) * (max - min) + 1);
 	let rolledDice = document.querySelector(`div[data-key="${roll}"]`);
-	console.log(rolledDice);
-	// rolledDice.style.classList.add("dice-active");
+
+	// Target all images, convert nodelist to array and loop over while deleting active state
+	let dice = document.querySelectorAll(".dice");
+	let diceArr = Array.from(dice);
+	diceArr.forEach(dice => {
+		dice.classList.remove("dice-active");
+	});
+
+	// Add active state to rolled dice and update text area
+	rolledDice.classList.add("dice-active");
 	textArea.innerHTML = `You rolled a ${roll}`;
-}
+});
